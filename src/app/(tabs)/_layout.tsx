e50ddd4010bdf39c23f5946a4e12e3e86644cc86
Uditya <uditya204@gmail.com>
2026-09-14 10:25:54 +0530

    Adjust tab bar height to account for safe area insets

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,17 +1,22 @@
 import Colors from "@/constants/Colors";
 import { Tabs } from "expo-router";
 import { House, Settings } from "lucide-react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const colors = Colors.light;
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets();
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
         tabBarActiveTintColor: colors.tabIconSelected,
         tabBarInactiveTintColor: colors.tabIconDefault,
-        tabBarStyle: { backgroundColor: colors.tabBackground },
+        tabBarStyle: {
+          backgroundColor: colors.tabBackground,
+          height: 60 + insets.bottom,
+        },
       }}
     >
       <Tabs.Screen
